7898cf1ace60f925d2d20f4cb91d9b81d462d1f4
Ahmet Emre Güzey <ahmeteg@stud.ntnu.no>
2022-05-21 20:26:53 +0200

    Added UnitFactory and fixed JavaFX

--- src/main/java/com/example/wargames/Controller.java
+++ src/main/java/com/example/wargames/Controller.java
@@ -1,35 +1,24 @@
 package com.example.wargames;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Controller extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-
-        Scene scene = new Scene(root, 300, 250);
-
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        try{
+            Parent root = FXMLLoader.load(getClass().getResource("BattleScene.fxml"));
+            Scene scene = new Scene(root, 300, 250);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
     }
     public static void main(String[] args) {
         launch(args);
---
+++ src/main/java/com/example/wargames/UnitFactory.java
@@ -0,0 +1,62 @@
+package com.example.wargames;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class UnitFactory {
+
+    final private String name;
+    private List<Unit> unitList;
+
+    public UnitFactory(String name){
+
+        this.name = name;
+        unitList = new ArrayList<Unit>();
+    }
+
+    public Unit createUnit(String type, String name, int health){
+        Unit newUnit = null;
+        if(type.equals("CavalryUnit")){
+            newUnit = new CavalryUnit(name, health);
+        }
+        if(type.equals("CommanderUnit")){
+            newUnit = new CommanderUnit(name, health);
+        }
+        if(type.equals("InfantryUnit")){
+            newUnit = new InfantryUnit(name, health);
+        }
+        if(type.equals("RangedUnit")){
+            newUnit = new RangedUnit(name, health);
+        }
+        return newUnit;
+    }
+
+    public List<Unit> unitSearch(String type, String name, int health){
+
+        List<Unit> searchRes = new ArrayList<Unit>();
+        //units.stream().filter(p -> p instanceof CommanderUnit ).collect(Collectors.toList());
+
+        for(int i = 0; i < unitList.size(); i++){
+            searchRes.add(unitList.get(i));
+        }
+
+        searchRes = searchRes.stream().filter(p -> p instanceof CavalryUnit ).filter(n -> n.getName().equals(name)).filter(h -> h.getHealth() == health).collect(Collectors.toList());
+
+        if(type.equals("CavalryUnit")){
+            searchRes = searchRes.stream().filter(p -> p instanceof CavalryUnit ).collect(Collectors.toList());
+        }
+        if(type.equals("CommanderUnit")){
+            searchRes = unitList.stream().filter(p -> p instanceof CommanderUnit ).collect(Collectors.toList());
+        }
+        if(type.equals("InfantryUnit")){
+            searchRes = unitList.stream().filter(p -> p instanceof InfantryUnit ).collect(Collectors.toList());
+        }
+        if(type.equals("RangedUnit")){
+            searchRes = unitList.stream().filter(p -> p instanceof RangedUnit ).collect(Collectors.toList());
+        }
+
+        return searchRes;
+    }
+
+}
